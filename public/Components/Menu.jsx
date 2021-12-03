d9e09f6a52d4e8272e38dca69062090097a1b5bc
Andres Atehortua <atehortu9620@gmail.com>
2021-12-02 23:22:20 -0500

    portafolio

---
+++ public/Components/Menu.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+export default function MyMenu(){
+
+    return(
+
+        <div className="menu-container"></div>
+
+    );
+
+}
---
+++ src/Components/Menu.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../Styles/Menu.css';
+
+export default function MyMenu(){
+    
+    return(
+
+        <div className="menu-container"></div>
+
+    );
+
+}

e3339f7d11fef77fb4382e6f6d37551dde153a8b
Andres Atehortua <atehortu9620@gmail.com>
2021-12-02 23:39:15 -0500

    background added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 import './Styles/App.css';
 
 import MyMenu from './Components/Menu';
+/* import Body from './Components/Body'; */
 
 function App() {
   return (
-    
+    <div className="App">
       
 
       <MyMenu/>
+      {/* <Body/> */}
+    </div>
     
   );
  
--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -7,6 +7,7 @@ export default function MyMenu(){
     return(
 
         <div className="menu-container"></div>
+        
 
     );
 

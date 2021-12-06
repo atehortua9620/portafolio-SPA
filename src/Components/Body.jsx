ac4da7d03a5bfe8f4a7cbff3fcfe1d349f7139bd
Andres Atehortua <atehortu9620@gmail.com>
2021-12-05 22:37:10 -0500

    skills section

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import '../Styles/Body.css';
+import css from '../assets_portafo/CSS.png';
+import html from '../assets_portafo/HTML.png';
+import rect from '../assets_portafo/react.png';
+import js from '../assets_portafo/JAVASCRIPT.png';
 
 export default function ImBody (){
 
     return(
         <div className="containerbody">
+            
+            <h2>SKILLS</h2>
+            <div className="techcontainer">
+                <img className="tech" src={css} alt="tech" />
+                <img className="tech" src={html} alt="tech" />
+                <img className="tech" src={rect} alt="tech" />
+                <img className="tech" src={js} alt="tech" />
+            </div>
 
 
         </div>

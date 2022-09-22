0b074477c1a796ac8e53c763d1ef740db9fdcb35
Andres Atehortua <atehortua9620@gmail.com>
2022-09-21 23:49:48 -0500

    errors fixed ready to update

--- src/Pages/Hi.jsx
+++ src/Pages/Hi.jsx
@@ -2,12 +2,7 @@ import React from "react";
 
 function hi(){
     return(
-<<<<<<< HEAD:src/Components/sidebar.jsx
         <div className="sideBarMenu">
-=======
-        <div>
-           
->>>>>>> 3486c7d:src/Pages/Hi.jsx
         </div>
     );
 }
